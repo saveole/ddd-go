e8cd69b88ec7b0a3aab7fe912208cfb37df55968
saveole <xgz.cz@foxmail.com>
2024-04-17 09:01:15 +0800

    setting the scene before ddd

---
+++ ch2/ch2.go
@@ -0,0 +1,50 @@
+package ch2
+
+import "context"
+
+type UserType = int
+type SubscriptionType = int
+
+const (
+	unknownUserType UserType = iota
+	lead
+	customer
+	churned
+	lostLead
+)
+
+const (
+	unknownSubscriptionType SubscriptionType = iota
+	basic
+	premium
+	exclusive
+)
+
+type UserAddRequest struct {
+	UserType       UserType
+	Email          string
+	SubType        SubscriptionType
+	PaymentDetails PaymentDetails
+}
+
+type UserModifyRequest struct {
+	ID             string
+	Email          string
+	UserType       UserType
+	SubType        SubscriptionType
+	PaymentDetails PaymentDetails
+}
+
+type User struct {
+	ID             string
+	PaymentDetails PaymentDetails
+}
+
+type PaymentDetails struct {
+	stripeTokenID string
+}
+
+type UserManager interface {
+	AddUser(ctx context.Context, request UserAddRequest) (User, error)
+	ModifyUser(ctx context.Context, request UserModifyRequest) (User, error)
+}
