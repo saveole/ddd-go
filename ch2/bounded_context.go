a82a009629038dd471071a6da8f4f472d8fa691c
saveole <xgz.cz@foxmail.com>
2024-04-18 14:05:33 +0800

    feat:bounded context change: A lead select a subscription than convert to a customer

--- ch2/bounded_context.go
+++ ch2/bounded_context.go
@@ -20,6 +20,40 @@ const (
 	exclusive
 )
 
+type LeadRequest struct {
+	email string
+}
+
+type Lead struct {
+	id string
+}
+
+type LeadCreator interface {
+	CreateLead(ctx context.Context, request LeadRequest) (Lead, error)
+}
+
+type Customer struct {
+	leadID string
+	userID string
+}
+
+func (c *Customer) UserID() string {
+	return c.userID
+}
+
+func (c *Customer) SetUserID(userID string) {
+	c.userID = userID
+}
+
+type LeadConvertor interface {
+	Convert(ctx context.Context, subSelection SubscriptionType) (Customer, error)
+}
+
+func (l *Lead) Convert(ctx context.Context, subSelection SubscriptionType) (Customer, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
 type UserAddRequest struct {
 	UserType       UserType
 	Email          string
